6648c32768a2d8e9abac5b46dcec60a044940fc4
felimarod <felipemartinrod@gmail.com>
2023-10-24 00:16:59 -0500

    Se termina el controlador de Tutorial

--- backend/services/tutorial_controlador.py
+++ backend/services/tutorial_controlador.py
@@ -7,3 +7,38 @@ def obtener_tutoriales():
     query = "SELECT id_tutorial, str_tema, str_descripcion, str_titulo, visible, id_detalle FROM Tutorial"
     cursor.execute(query)
     return cursor.fetchall()
+
+
+def obtener_tutorial_por_id(id):
+    db = get_db()
+    cursor = db.cursor()
+    statement = "SELECT id_tutorial, str_tema, str_descripcion, str_titulo, visible, id_detalle FROM Tutorial WHERE id_tutorial = ?"
+    cursor.execute(statement, [id])
+    return cursor.fetchone()
+
+
+def agregar_tutorial(tema, descripcion, titulo, visible, id_detalle):
+    db = get_db()
+    cursor = db.cursor()
+    statement = "INSERT INTO Tutorial ( str_tema, str_descripcion, str_titulo, visible, id_detalle ) VALUES ( ?, ?, ?, ?, ? )"
+    cursor.execute(statement, [tema, descripcion, titulo, visible, id_detalle])
+    db.commit()
+    return True
+
+
+def actualizar_tutorial(id, tema, descripcion, titulo, visible, id_detalle):
+    db = get_db()
+    cursor = db.cursor()
+    statement = "UPDATE Tutorial SET str_tema = ?, str_descripcion = ?, str_titulo = ?, visible = ?, id_detalle = ? WHERE id_tutorial = ?"
+    cursor.execute(statement, [tema, descripcion, titulo, visible, id_detalle, id])
+    db.commit()
+    return True
+
+
+def eliminar_tutorial_por_id(id):
+    db = get_db()
+    cursor = db.cursor()
+    statement = "DELETE FROM Tutorial WHERE id = ?"
+    cursor.execute(statement, [id])
+    db.commit()
+    return True
