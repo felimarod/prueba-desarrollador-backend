ddb906c0b32fe08f27fee78fd2845f5a930575c6
felimarod <felipemartinrod@gmail.com>
2023-10-24 16:48:34 -0500

    agregar soporte cors

--- app.py
+++ app.py
@@ -58,10 +58,22 @@ def eliminar_tutorial(id):
     result = eliminar_tutorial_por_id(id)
     return jsonify(result)
 
+
 @app.route('/api/detalle/<id>', methods=["GET"])
 def get_detalle(id):
     detalle = obtener_detalle_por_id(id)
     return jsonify(detalle)
+
+
+@app.after_request
+def after_request(response):
+    # <- You can change "*" for a domain for example "http://localhost"
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Credentials"] = "true"
+    response.headers["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS, PUT, DELETE"
+    response.headers["Access-Control-Allow-Headers"] = "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization"
+    return response
+
 
 if __name__ == "__main__":
     crear_tablas()
